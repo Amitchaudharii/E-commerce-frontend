f8eab255cc13faec44f62eb61f7bc025d88faa90
Amit chaudhary <chaudhary.amit2058@gmail.com>
2022-12-18 01:52:34 +0545

    Reducer set Products, featureProduct

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -1,22 +1,40 @@
 import axios from "axios";
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import reducer from "../reducer/productReducer";
 
 const AppContext = createContext();
 
 // Main API
 const API = "https://api.pujakaitem.com/api/products";
 
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+};
+
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const getProducts = async (url) => {
-    const res = await axios.get(url);
-    const products = await res.data;
+    dispatch({ type: "SEET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
   };
 
   useEffect(() => {
     getProducts(API);
   }, []);
 
-  return <AppContext.Provider value="Amit">{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
 
 // Custom Hooks
